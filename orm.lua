4dba9a96050074c530761b2943b3788b43a7b5ce
huangchanglong <huangchanglong@ijunhai.com>
2023-01-01 18:24:47 +0800

    #init ant lua orm

---
+++ orm.lua
@@ -0,0 +1,467 @@
+-- lua function enable_oldindex can enable or disable metamethod __oldindex
+local enable_oldindex = enable_oldindex or (function() end)
+
+local tonumber = tonumber
+local tostring = tostring
+
+local KEYWORD_ATTRS = {
+    ['__cls'] = true
+}
+
+-- 业务注册对应类型 integer boolean string binary map list struct date
+local KEYWORD_MAP = {}
+local function register_type(cls_type, is_atom, parse, default)
+    KEYWORD_MAP[cls_type] = {is_atom = is_atom, parser = parse, default = default}
+end
+
+local function has_cls_type(cls_type)
+    return KEYWORD_MAP[cls_type]
+end
+
+local function get_register_cls(cls_type)
+    return KEYWORD_MAP[cls_type]
+end
+
+local function get_default(cls_type)
+    return KEYWORD_MAP[cls_type].default
+end
+
+local g_cls_ref_map = {} -- cls_name : [parent_name, ...]
+local g_cls_map = {} -- cls_name: cls
+
+local function check_ref(node_id, parent_id)
+    -- print('check ref', node_id, parent_id)
+    if parent_id == nil then
+        return
+    end
+
+    if parent_id == node_id then
+        error(string.format('type<%s> ref redefined', node_id))
+    end
+
+    local p_map = g_cls_ref_map[node_id]
+    if not p_map then
+        p_map = {}
+        g_cls_ref_map[node_id] = p_map
+    end
+
+    p_map[parent_id] = true -- record parent
+
+    -- check and update parent's parent
+    local pp_map = g_cls_ref_map[parent_id]
+    if not pp_map then
+        pp_map = {}
+        g_cls_ref_map[parent_id] = pp_map
+    end
+
+    for pp_id, _ in pairs(pp_map) do
+        check_ref(node_id, pp_id)
+    end
+end
+
+------------ parse begin -----------
+local function _cls_parse_error(cls, data, msg)
+    local s = string.format("cls parse: <%s> <%s> %s", cls.name, data, msg)
+    error(s)
+end
+
+local function parse_boolean(_, s)
+    return s == true
+end
+
+local function parse_string(_, s)
+    return tostring(s)
+end
+
+local function parse_integer(cls, s)
+    local value = math.tointeger(s)
+    if value == nil then
+        _cls_parse_error(cls, s, "is not integer")
+    end
+
+    return value
+end
+
+local function parse_double(cls, s)
+    local value = tonumber(s)
+    if value == nil then
+        _cls_parse_error(cls, s, "is not double")
+    end
+    return value
+end
+
+local function parse_binary(_, s)
+    return s
+end
+
+local function parse_date(_, s)
+    return s
+end
+
+local function parse_struct(cls, data)
+    if data == nil then
+        return cls:new()
+    end
+
+    if type(data) ~= 'table' then
+        _cls_parse_error(cls, data, "is not table")
+    end
+
+    local ret = {}
+    for attr_name, attr_cls in pairs(cls.attrs) do
+        local attr_data = data[attr_name]
+        -- 是否需要初始化默认值？
+        if not (attr_data == nil and attr_cls.is_atom) then
+            ret[attr_name] = attr_cls:parse(attr_data)
+        end
+    end
+    -- print('parse struct create obj', cls.name, ret)
+    return cls:new(ret)
+end
+
+local function parse_list(cls, data)
+    if data == nil then
+        return cls:new()
+    end
+
+    if type(data) ~= 'table' then
+        _cls_parse_error(cls, data, "is not table")
+    end
+
+    local ret = {}
+    local item_cls = cls.item
+    for _, _data in ipairs(data) do
+        table.insert(ret, item_cls:parse(_data))
+    end
+    return cls:new(ret)
+end
+
+local function parse_map(cls, data)
+    if data == nil then
+        return cls:new()
+    end
+
+    if type(data) ~= 'table' then
+        _cls_parse_error(cls, data, "is not table")
+    end
+
+    --local cls_name = cls.name
+    local k_cls = cls.key
+    local v_cls = cls.value
+    local ret = {}
+    for k_data, v_data in pairs(data) do
+        ret[k_cls:parse(k_data)] = v_cls:parse(v_data)
+    end
+    return cls:new(ret)
+end
+------------ parse end -----------
+
+---------------create begin ---------------
+local function create_struct(cls, data)
+    local obj = {}
+    enable_oldindex(obj, true)
+    setmetatable(obj, cls.mt)
+
+    -- check data type
+    if data == nil then
+        for k, v in pairs(cls.attrs) do
+            if not v.is_atom then
+                obj[k] = nil
+            end
+        end
+    else
+        for k, v in pairs(cls.attrs) do
+            local k_data = data[k]
+            if not (k_data == nil and v.is_atom) then
+                obj[k] = k_data
+            end
+        end
+    end
+    return obj
+end
+
+local function create_list(cls, data)
+    local obj = {}
+    enable_oldindex(obj, true)
+    setmetatable(obj, cls.mt)
+
+    if data == nil then
+        return obj
+    end
+
+    for idx, item in ipairs(data) do
+        obj[idx] = item
+    end
+    return obj
+end
+
+local function create_map(cls, data)
+    local obj = {}
+    enable_oldindex(obj, true)
+    setmetatable(obj, cls.mt)
+
+    if data == nil then
+        return obj
+    end
+
+    for k, v in pairs(data) do
+        obj[k] = v
+    end
+    return obj
+end
+---------------create finish ---------------
+
+--------------- setfield begin ------------
+local function struct_setfield(obj, k, v)
+    local cls = obj.__cls
+    if not cls then
+        error(string.format("struct no cls info"))
+    end
+
+    local v_cls = cls.attrs[k]
+    if not v_cls then
+        error(string.format('cls struct: <%s> has no attr<%s>', cls.name, k))
+    end
+
+    -- optimize, trust cls obj by name
+    if type(v) == 'table' and v.__cls ~= nil then
+        if v_cls.id == v.__cls.id then
+            -- print(
+            --     '-- struct trust cls obj',
+            --     cls.name, k, v_cls.name, v.__cls
+            -- )
+            rawset(obj, k, v)
+            return
+        end
+        local s = string.format(
+            'cls struct: <%s.%s> value type not match need<%s> give<%s>',
+            cls.name, k, v_cls.id.name, v.__cls.name
+        )
+        error(s)
+    end
+
+    -- if v == nil, set node default
+    -- print('-- struct, paser ', cls.name, k, v, v_cls.name, v_cls.parser)
+    if v == nil and v_cls.is_atom then
+        rawset(obj, k, nil)
+        return
+    end
+
+    rawset(obj, k, v_cls:parse(v))
+end
+
+local function list_setfield(obj, k, v)
+    -- print('list __newindex', obj, k, v)
+    local cls = obj.__cls
+    if not cls then
+        error(string.format("list no cls info"))
+    end
+
+    if k ~= math.tointeger(k) then
+        local s = string.format('cls list: <%s.%s> = %s is not integer index', cls.name, k, v)
+        error(s)
+    end
+
+    if v == nil then -- if v == nil, remove node
+        rawset(obj, k, nil)
+        return
+    end
+
+    local v_cls = cls.item
+    -- optimize, trust cls obj by name
+    if type(v) == 'table' and v.__cls ~= nil then
+        if v_cls.id == v.__cls.id then
+            -- print(
+            --     '-- list trust cls obj',
+            --     cls.name, k, v_cls.name, v.__cls
+            -- )
+            rawset(obj, k, v)
+            return
+        end
+        local s = string.format(
+            'cls list: <%s.%s> value type not match need<%s> give<%s>',
+            cls.name, k, v_cls.id.name, v.__cls.name
+        )
+        error(s)
+    end
+
+    rawset(obj, k, v_cls:parse(v))
+end
+
+local function map_setfield(obj, k, v)
+    -- print('map __newindex', obj, k, v)
+    local cls = obj.__cls
+    if not cls then
+        error(string.format("cls map invalid to set <nil.%s> = %s", k, v))
+    end
+
+    local k_data = cls.key:parse(k)
+    if v == nil then
+        -- remove node
+        rawset(obj, k_data, nil)
+        return
+    end
+
+    local v_cls = cls.value
+    -- optimize, trust cls obj by name
+    if type(v) == 'table' and v.__cls ~= nil then
+        if v_cls.id == v.__cls.id then
+            -- print(
+            --     '-- map trust cls obj',
+            --     cls.name, k, v_cls.name, v.__cls
+            -- )
+            rawset(obj, k_data, v)
+            return
+        end
+
+        local s = string.format(
+            'cls map:<%s.%s> value type not match need<%s> give<%s>',
+            cls.name, k_data, v_cls.id.name, v.__cls.name
+        )
+        error(s)
+    end
+
+    rawset(obj, k_data, v_cls:parse(v))
+end
+------------- setfield end ---------
+
+
+local function create_cls(cls, parent_name)
+    if parent_name then
+        cls.name = parent_name .. "." .. cls.name
+    end
+
+    local cls_name = cls.name
+    local cls_type = cls.type
+
+    if has_cls_type(cls_name) then
+        error(string.format("cls name<%s> is keyword", cls_name))
+        return nil
+    end
+
+    if not cls_type then
+        error(string.format(" cls name<%s> invalid cls type", cls_name))
+        return nil
+    end
+
+    -- ref type
+    local ref_cls = g_cls_map[cls_type]
+    if ref_cls then
+        check_ref(ref_cls.name, parent_name)
+        -- copy ref
+        for k, v in pairs(ref_cls) do
+            cls[k] = v
+        end
+        -- reset name
+        cls.name = cls_name
+        if not ref_cls.id then
+            assert(false, cls.name)
+            cls.id = ref_cls
+        end
+        return cls
+    end
+
+    local reg_cls = get_register_cls(cls_type)
+    if not (reg_cls and reg_cls.parse) then
+        error(string.format("data type <%s> <%s> no parse", cls_name, cls_type))
+        return nil
+    end
+
+    check_ref(cls_name, parent_name)
+
+    cls.id = cls
+    cls.parse = reg_cls.parser
+    cls.is_atom = reg_cls.is_atom
+
+    if cls.is_atom then
+        return cls
+    end
+
+    if cls_type == 'struct' then
+        cls.new = create_struct
+        local mt_index = {__cls = cls}
+        cls.mt = {
+            __index = mt_index,
+            __newindex = struct_setfield,
+            __oldindex = struct_setfield,
+        }
+        assert(cls.attrs, "not attrs")
+        local attrs = {}
+        for k, v in pairs(cls.attrs) do
+            if KEYWORD_ATTRS[k] then
+                error(string.format("class <%s> define key attr <%s>", cls_name, k))
+            end
+            v.name = k
+            local v_cls = create_cls(v, cls_name)
+            if v_cls.is_atom then
+                -- set default in meta index
+                mt_index[k] = get_default(v_cls.type)
+            end
+            attrs[k] = v_cls
+        end
+        cls.attrs = attrs
+        return cls
+    end
+
+    if cls_type == 'list' then
+        cls.new = create_list
+        cls.mt = {
+            __index = {__cls = cls},
+            __newindex = list_setfield,
+            __oldindex = list_setfield,
+        }
+        cls.item.name = 'item'
+        cls.item = create_cls(cls.item, cls_name)
+        return cls
+    end
+
+    if cls_type == 'map' then
+        cls.new = create_map
+        cls.mt = {
+            __index = {__cls = cls},
+            __newindex = map_setfield,
+            __oldindex = map_setfield,
+        }
+        cls.key.name = 'key'
+        cls.key = create_cls(cls.key, cls_name)
+        cls.value.name = 'value'
+        cls.value = create_cls(cls.value, cls_name)
+        return cls
+    end
+    error(string.format("unsupport cls type<%s>", cls_type))
+end
+
+register_type("boolean", true, parse_boolean, false)
+register_type("integer", true, parse_integer, 0)
+register_type("double", true, parse_double, 0)
+register_type("string", true, parse_string, "")
+register_type("binary", true, parse_binary, "")
+register_type("date", true, parse_date, nil) -- mongodb ISODate bson.date(timestamp)
+register_type("map", false, parse_map, nil)
+register_type("list", false, parse_list, nil)
+register_type("struct", false, parse_struct, nil)
+
+local M = {}
+function M.init(type_list)
+    -- reset
+    g_cls_map = {}
+    g_cls_ref_map = {}
+
+    for _, item in ipairs(type_list) do
+        local name = item.name
+        assert(name, 'not cls name')
+        g_cls_map[name] = create_cls(item, nil)
+    end
+    M.g_cls_map = g_cls_map
+    M.g_cls_ref_map = g_cls_ref_map
+end
+
+function M.create(cls_name, data)
+    local cls = g_cls_map[cls_name]
+    if not cls then
+        error(string.format("create obj, illgeal cls<%s>", cls_name))
+    end
+    return cls:new(data)
+end
+
+return M
---
+++ typedef.lua
@@ -0,0 +1,336 @@
+local lpeg = require "lpeg"
+local P = lpeg.P
+local R = lpeg.R
+local S = lpeg.S
+local C = lpeg.C
+local Ct = lpeg.Ct
+local V = lpeg.V
+local Cc = lpeg.Cc
+local Cg = lpeg.Cg
+local Carg = lpeg.Carg
+
+local line_infos = {}
+local function count_lines(_,pos, parser_state)
+    if parser_state.pos < pos then
+        parser_state.line = parser_state.line + 1
+        parser_state.pos = pos
+    end
+    return pos
+end
+
+
+local color = {
+    red = 31,
+    green = 32,
+    blue = 36,
+    yellow = 33,
+    other = 37
+}
+local function highlight(s, c)
+    c = c or "red"
+    return string.format("\x1b[1;%dm%s\x1b[0m", color[c], tostring(s))
+end
+
+local function highlight_type(s)
+    return highlight(s, "green")
+end
+
+local function highlight_tag(s)
+    return highlight(s, "yellow")
+end
+
+local exception = lpeg.Cmt(
+    Carg(1),
+    function(_, pos, parser_state)
+        local line_info = line_infos[parser_state.line]
+        local s = highlight(string.format("syntax error at %s:%d line", line_info.file, line_info.line))
+        error(s)
+        return pos
+    end
+)
+
+local eof = P(-1)
+local newline = lpeg.Cmt((P"\n" + "\r\n") * Carg(1) ,count_lines)
+local line_comment = "#" * (1 - newline) ^0 * (newline + eof)
+local blank = S" \t" + newline + line_comment
+local blank0 = blank ^ 0
+local blanks = blank ^ 1
+local alpha = R"az" + R"AZ" + "_"
+local alnum = alpha + R"09"
+local word = alpha * alnum ^ 0
+local name = C(word)
+local typename = C(word * ("." * word) ^ 0)
+--local tag = R"09" ^ 1 / tonumber
+--local mainkey = "(" * blank0 * name * blank0 * ")"
+--local decimal = "(" * blank0 * C(tag) * blank0 * ")"
+
+local function metapatt(_)
+    local patt = lpeg.Cmt(
+        Carg(1),
+        function(_, pos, parser_state)
+            local info = line_infos[parser_state.line]
+            setmetatable(info, {__tostring = function(v)
+                return highlight(string.format("at %s:%d line", v.file, v.line))
+            end})
+            return pos, info
+        end
+    )
+    return patt
+end
+
+local function multipat(pat)
+    return Ct(blank0 * (pat * blanks) ^ 0 * pat^0 * blank0)
+end
+
+local function namedpat(patname, pat)
+    local pattype = Cg(Cc(patname), "type")
+    local meta = Cg(metapatt(patname), "meta")
+    return Ct(pattype * meta * Cg(pat))
+end
+
+local typedef = P {
+    "ALL",
+
+    FIELD = namedpat(
+        "field",
+        (name * blanks * ":" * blank0 *
+             (
+                 namedpat(
+                     "ref",
+                     typename
+                 ) +
+
+                 namedpat(
+                     "list",
+                     '*' * blank0 * typename
+                 ) +
+
+                 namedpat(
+                     "map",
+                      P"<" * blank0 * typename * blank0 * "," * blank0 * typename * blank0 * P">"
+                 )
+             )
+        )
+        +
+        (P"." * name * blanks *
+            namedpat(
+                "struct",
+                P"{" * multipat(V"FIELD") * P"}"
+            )
+        )
+    ),
+
+    STRUCT = namedpat(
+        "struct",
+        blank0 * P"." * name * blank0 * P"{" * multipat(V"FIELD") * P"}"
+    ),
+
+    LIST = namedpat(
+        "list",
+        blank0 * name * blank0 * ":" * blank0 * "*" * blank0 * typename
+    ),
+
+    MAP = namedpat(
+        "map",
+        blank0 * name * blank0 * ":" *
+            blank0 * P"<" * blank0 * typename * blank0 * "," * blank0 * typename * blank0 * P">"
+    ),
+
+    ALL = multipat(V"STRUCT" + V"LIST" + V"MAP"),
+}
+
+local schema = blank0 * typedef * blank0
+
+local function preprocess(filename, dir)
+    local text = {}
+    local path = dir .. "/" .. filename
+    line_infos = {}
+    local idx = 0
+    for line in io.lines(path) do
+        idx = idx + 1
+        local include = string.match(line, "^%s*#include%s+([^%s]+)%s*")
+        if not include then
+            local _idx = #text + 1
+            text[_idx] = line
+            line_infos[_idx] = {line = idx, file=path}
+        else
+            local _idx = 0
+            include = dir .. "/" .. include
+            for _line in io.lines(include) do
+                _idx = _idx + 1
+                local len = #text+1
+                text[len] = _line
+                line_infos[len] = {line = _idx, file=include}
+            end
+        end
+    end
+    return table.concat(text, "\n")
+end
+
+local KEYWORD_MAP = {
+    -- 类型关键词
+    boolean = true,
+    integer = true,
+    string = true,
+    struct = true,
+    list = true,
+    map = true,
+    double = true,
+    binary = true,
+    date = true,
+
+    -- 业务属性关键词禁止定义为key
+    __cls = true,
+}
+
+local function has_type(type, extra_types)
+    if not type then
+        return false
+    end
+    if KEYWORD_MAP[type] then
+        return true
+    end
+    return extra_types[type] and true or false
+end
+
+local convert = {}
+function convert.struct(obj, set)
+    local type_name = obj[1]
+    if KEYWORD_MAP[type_name] then
+        error(string.format("type_name<%s> is keyword", highlight_type(type_name)))
+    end
+
+    local field_map = {}
+    for _, f in ipairs(obj[2]) do
+        local field_name = f[1]
+        local field_data = f[2]
+        if(type(field_data) ~= 'table') then
+            field_data = f[3]
+        end
+
+        if field_data == '*' then
+            field_data = f[4]
+        end
+        local field_data_type = field_data.type
+
+        if KEYWORD_MAP[field_name] then
+            error(string.format("struct %s field %s is keyword", highlight_type(type_name), highlight_tag(field_name)))
+        end
+
+        if field_map[field_name] then
+            error(string.format("struct %s field %s is redefined", highlight_type(type_name), highlight_tag(field_name)))
+        end
+
+        local field = {}
+        if field_data_type == 'ref' then
+            if not has_type(field_data[1], set) then
+                error(string.format("Undefined type <%s> %s", highlight_type(field_data[1]), tostring(obj.meta)))
+            end
+            field.type = field_data[1]
+        elseif field_data_type == 'map' then
+            field.type = 'map'
+
+            if not has_type(field_data[1], set) then
+                error(string.format("Undefined type <%s> in %s", highlight_type(field_data[1]), tostring(obj.meta)))
+            end
+            if not has_type(field_data[2], set) then
+                error(string.format("Undefined type <%s> in %s", highlight_type(field_data[2]), tostring(obj.meta)))
+            end
+
+            field.key = {type = field_data[2]}
+            field.value = {type = field_data[1]}
+
+        elseif field_data_type == 'list' then
+            field.type = 'list'
+            if not has_type(field_data[1], set) then
+                error(string.format("Undefined type <%s> in %s", highlight_type(field_data[1]), tostring(obj.meta)))
+            end
+            field.item = {type = field_data[1]}
+
+        elseif field_data_type == 'struct' then
+            local sub_struct_obj = {field_name, field_data[1], type = 'struct', meta = field_data.meta}
+            for k, v in pairs(convert.struct(sub_struct_obj, set)) do
+                field[k] = v
+            end
+            field.name = nil
+        else
+            error(string.format("struct %s field %s unknown type", highlight_type(type_name), highlight_tag(field_name)))
+        end
+
+        field_map[field_name] = field
+    end
+
+    return {
+        type = 'struct',
+        name = type_name,
+        attrs = field_map,
+    }
+end
+
+function convert.list(obj, set)
+    local type_name = obj[1]
+    if KEYWORD_MAP[type_name] then
+        error(string.format("type_name<%s> is keyword", highlight_type(type_name)))
+    end
+
+    if not has_type(obj[4], set) then
+        error(string.format("Undefined type <%s> in %s", highlight_type(obj[4]), tostring(obj.meta)))
+    end
+
+    return {
+        type = 'list',
+        name = type_name,
+        item = {type = obj[4]},
+    }
+end
+
+function convert.map(obj, set)
+    local type_name = obj[1]
+    if KEYWORD_MAP[type_name] then
+        error(string.format("type_name<%s> is keyword", highlight_type(type_name)))
+    end
+
+    if not has_type(obj[5], set) then
+        error(string.format("Undefined type <%s> in %s", highlight_type(obj[5]), tostring(obj.meta)))
+    end
+    if not has_type(obj[4], set) then
+        error(string.format("Undefined type <%s> in %s", highlight_type(obj[4]), tostring(obj.meta)))
+    end
+
+    return {
+        type = 'map',
+        name = type_name,
+        key = {type = obj[5]},
+        value = {type = obj[4]},
+    }
+end
+
+local tprint = require('utils').dump
+local function parse(pattern, filename, dir)
+    assert(type(filename) == "string")
+    local text = preprocess(filename, dir)
+    local state = {file = filename, pos = 0, line = 1}
+    local r = lpeg.match(pattern * -1 + exception, text, 1, state)
+    local ret = {}
+    -- optimize 可以考虑支持namespace 同一个namespac下禁止同名
+    local set = {}
+    for _, item in ipairs(r) do
+        local type_name = item[1]
+        if set[type_name] ~= nil then
+            local meta_info = tostring(item.meta)
+            assert(false, string.format("redefine <%s> %s", highlight_type(type_name), highlight(meta_info)))
+        end
+        table.insert(ret, convert[item.type](item, set))
+        set[type_name] = true
+    end
+
+    return ret
+end
+
+
+local M = {}
+function M.parse(...)
+    return parse(schema, ...)
+end
+
+return M
